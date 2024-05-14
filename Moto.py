427d9ed074f65ceb86c3f78ac0456df960e90249
JoaoPVCosta <152276803+JoaoPVCosta@users.noreply.github.com>
2024-05-14 17:48:49 +0100

    feat(main): criar método da api para as motas

--- Moto.py
+++ Moto.py
@@ -1,8 +1,12 @@
 from Vehicle import Vehicle
+from pydantic import BaseModel
 
 
-class Moto(Vehicle):
+class Moto(Vehicle, BaseModel):
     sidecar: str
 
     def __str__(self):
         return f"{super().__str__()} \n Sidecar: {self.sidecar}"
+
+    def saveData(self):
+        print("Moto added successfuly!")
--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from Car import Car
+from Moto import Moto
 
 app = FastAPI()
 
@@ -11,3 +12,8 @@ async def root():
 async def add_car(car: Car):
     car.saveData()
     return {"ID": car.id, "Brand": car.brand, "Model": car.model, "Year": car.year, "Kms": car.kms, "Horsepower": car.hp, "Price": car.price, "Color": car.color, "Doors": car.doors}
+
+@app.post("/addMoto/")
+async def add_moto(moto: Moto):
+    moto.saveData()
+    return {"ID": moto.id, "Brand": moto.brand, "Model": moto.model, "Year": moto.year, "Kms": moto.kms, "Horsepower": moto.hp, "Price": moto.price, "Color": moto.color, "Sidecar": moto.sidecar}
